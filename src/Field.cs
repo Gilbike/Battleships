6bd61c1cfbbf4f4a23f66c5b3f1499ab9d2604b4
Gilbike <marton.dikacz@gmail.com>
2023-05-20 21:54:05 +0200

    add ship and hit render states

--- src/Field.cs
+++ src/Field.cs
@@ -3,27 +3,40 @@ using Microsoft.Xna.Framework;
 
 namespace Battleships;
 
-enum FieldState {
+public enum FieldState {
   Empty,
   Ship,
-  Hit
+  Hit,
+  ShipHit,
 }
 
 public class Field : BaseObject {
   private Texture2D _texture;
   private Vector2 _position;
 
-  private FieldState _state;
+  public FieldState State { get; set; }
 
   public Field(Vector2 position, Texture2D texture) {
     _position = position;
     _texture = texture;
 
-    _state = FieldState.Empty;
+    State = FieldState.Empty;
   }
 
   public void Render(SpriteBatch batch) {
-    batch.Draw(_texture, _position, Color.White);
+    Color renderColor = Color.White;
+    switch (State) {
+      case FieldState.Ship:
+        renderColor = Color.GreenYellow;
+        break;
+      case FieldState.Hit:
+        renderColor = Color.Orange;
+        break;
+      case FieldState.ShipHit:
+        renderColor = Color.Red;
+        break;
+    }
+    batch.Draw(_texture, _position, renderColor);
   }
 
   public void Update() { }
--- src/Grid.cs
+++ src/Grid.cs
@@ -21,11 +21,11 @@ public class Grid : BaseObject {
     _encoded = encoded;
     _fieldSize = (size - (FieldGap - 1) * 10) / 10;
 
-    createFieldTexture(device);
-    createFields();
+    CreateFieldTexture(device);
+    CreateFields();
   }
 
-  private void createFieldTexture(GraphicsDevice device) {
+  private void CreateFieldTexture(GraphicsDevice device) {
     _fieldTexture = new Texture2D(device, _fieldSize, _fieldSize);
     Color[] data = new Color[_fieldSize * _fieldSize];
     for (int i = 0; i < _fieldSize * _fieldSize; i++) {
@@ -34,7 +34,7 @@ public class Grid : BaseObject {
     _fieldTexture.SetData(data);
   }
 
-  private void createFields() {
+  private void CreateFields() {
     for (int row = 0; row < 10; row++) {
       for (int col = 0; col < 10; col++) {
         int rowGap = (row - 1) * FieldGap;
@@ -42,6 +42,12 @@ public class Grid : BaseObject {
         _fields[col + row * 10] = new Field(new Vector2(_position.X + col * _fieldSize + colGap, _position.Y + row * _fieldSize + rowGap), _fieldTexture);
       }
     }
+
+    _fields[0].State = FieldState.Ship;
+    _fields[10].State = FieldState.Ship;
+    _fields[20].State = FieldState.Ship;
+    _fields[1].State = FieldState.Hit;
+    _fields[30].State = FieldState.ShipHit;
   }
 
   public void Render(SpriteBatch batch) {
@@ -50,6 +56,5 @@ public class Grid : BaseObject {
     }
   }
 
-  public void Update() {
-  }
+  public void Update() { }
 }
