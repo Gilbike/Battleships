7e24d591632529c2b704569c2ec6349c13e903aa
Gilbike <marton.dikacz@gmail.com>
2023-05-23 21:23:25 +0200

    ai now uniformly attacks

--- src/OpponentAI.cs
+++ src/OpponentAI.cs
@@ -1,5 +1,8 @@
 using System;
+using System.Linq;
 using System.Threading.Tasks;
+using System.Collections.Generic;
+using Microsoft.Xna.Framework;
 
 namespace Battleships;
 
@@ -12,16 +15,42 @@ public class OpponentAI {
   private LineAttack lineAttack = new LineAttack();
 
   private List<int> attackedFields = new List<int>();
+
+  private int attacksSinceCheck = 0;
+  private int[] quarters = new int[4];
+
   public OpponentAI(Grid playerGrid) {
     random = new Random();
     _playerGrid = playerGrid;
   }
 
-  public void Update() { }
+  private void CalculateQuarters() {
+    for (int i = 0; i < 4; i++) {
+      int quarter = 0;
+
+      int minRow = i < 2 ? 0 : 5;
+      int maxRow = i < 2 ? 5 : 10;
+
+      int minCol = i % 2 == 0 ? 0 : 5;
+      int maxCol = i % 2 == 0 ? 5 : 10;
+
+      for (int row = minRow; row < maxRow; row++) {
+        for (int col = minCol; col < maxCol; col++) {
+          int index = _playerGrid.GetIndexFromLocationVector(new Vector2(col, row));
+          if (attackedFields.IndexOf(index) != -1) {
+            quarter++;
+          }
+        }
+      }
+      System.Console.WriteLine("quarter " + i + " got " + quarter + " attacks");
+      quarters[i] = quarter;
+    }
+  }
 
   public async Task<bool> AttackPlayer() {
     await Task.Delay(random.Next(500, 1750));
-    int selectedField = random.Next(100);
+
+    int selectedField;
     bool result = false;
 
     int selectedDirection = -1;
@@ -29,35 +58,50 @@ public class OpponentAI {
     do {
       selectedField = random.Next(100);
 
-    if (_lastResult.successful || lineAttack.enabled) { // select neighbour field when hit successful
-      int[] neighbours = _playerGrid.GetNeighbourFields(_lastResult.location);
-      if (!lineAttack.enabled || lineAttack.searchingForDirection) {
-        do {
-          selectedDirection = random.Next(neighbours.Length);
-        } while (neighbours[selectedDirection] == -1);
-        selectedField = neighbours[selectedDirection];
-        if (neighbours[selectedDirection] == -1) { // hit border
-          selectedField = random.Next(100);
-          lineAttack = new LineAttack();
-        }
-      } else if (lineAttack.enabled && !lineAttack.searchingForDirection) {
-        selectedField = neighbours[lineAttack.direction];
+      if (_lastResult.successful || lineAttack.enabled) { // select neighbour field when hit successful
+        int[] neighbours = _playerGrid.GetNeighbourFields(_lastResult.location);
+        if (!lineAttack.enabled || lineAttack.searchingForDirection) {
+          do {
+            selectedDirection = random.Next(neighbours.Length);
+          } while (neighbours[selectedDirection] == -1);
+          selectedField = neighbours[selectedDirection];
           if (neighbours[selectedDirection] == -1) { // hit border
             selectedField = random.Next(100);
             lineAttack = new LineAttack();
-      }
-    }
+          }
+        } else if (lineAttack.enabled && !lineAttack.searchingForDirection) {
+          selectedField = neighbours[lineAttack.direction];
+          if (neighbours[lineAttack.direction] == -1) { // hit border
+            selectedField = random.Next(100);
+            lineAttack = new LineAttack();
+          }
+        }
       }
     } while (attackedFields.IndexOf(selectedField) != -1);
+
+    if (!lineAttack.enabled && selectedDirection == -1 && attacksSinceCheck >= 5) {
+      CalculateQuarters();
+      int laggingRegionIndex = quarters.ToList().IndexOf(quarters.Min());
+
+      int minRow = laggingRegionIndex < 2 ? 0 : 5;
+      int maxRow = laggingRegionIndex < 2 ? 5 : 10;
+
+      int minCol = laggingRegionIndex % 2 == 0 ? 0 : 5;
+      int maxCol = laggingRegionIndex % 2 == 0 ? 5 : 10;
+
+      selectedField = _playerGrid.GetIndexFromLocationVector(new Vector2(random.Next(minCol, maxCol), random.Next(minRow, maxRow)));
+
+      attacksSinceCheck = 0;
+      System.Console.WriteLine("random lagging area " + laggingRegionIndex);
+    }
 
     result = _playerGrid.AttackField(selectedField);
     attackedFields.Add(selectedField);
 
     if (_lastResult.successful && !result) { // last one was successful but this one was not
       if (lineAttack.enabled) {
-        System.Console.WriteLine();
         lineAttack = new LineAttack();
-      } else {
+      } else if (!lineAttack.enabled && selectedDirection != -1) {
         lineAttack.enabled = true;
         lineAttack.searchedDirections[selectedDirection] = true;
         selectedField = _lastResult.location;
@@ -74,6 +118,8 @@ public class OpponentAI {
     }
 
     _lastResult = new LastAttackResult { location = selectedField, successful = result };
+
+    attacksSinceCheck++;
 
     return await Task.Run<bool>(delegate () {
       return result;
