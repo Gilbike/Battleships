d0582c523d3931b1dca41bf71e01c3ffbb9a8c43
Gilbike <marton.dikacz@gmail.com>
2023-06-05 17:38:24 +0200

    add toggle ui element

--- src/UI/SettingsMenu.cs
+++ src/UI/SettingsMenu.cs
@@ -7,6 +7,7 @@ public class SettingsMenu : UIScreen {
   private static int totalHeight = 35 + 3 + 35 + 2 + 30;
 
   private UIButton backButton;
+  private UIToggle soundToggle;
 
   public Action BackRequested;
 
@@ -18,6 +19,9 @@ public class SettingsMenu : UIScreen {
     backButton = new UIButton(UIManager.ScreenCenter - new Vector2(100, settingsTitleSize.Y / 2 - (totalHeight / 2) + 30), new Vector2(200, 30), "Back");
     backButton.OnButtonPressed += delegate () { BackRequested?.Invoke(); };
 
+    soundToggle = new UIToggle(UIManager.ScreenCenter - new Vector2(-115, settingsTitleSize.Y / 2 - (totalHeight / 2) + 67), new Vector2(35, 35));
+
     Elements.Add(backButton);
+    Elements.Add(soundToggle);
   }
 }
---
+++ src/UI/UIToggle.cs
@@ -0,0 +1,33 @@
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Battleships.Resources;
+
+namespace Battleships.UI;
+
+public class UIToggle : UIElement {
+  private readonly Color foreground = new Color(51, 51, 51);
+  private readonly Color foregroundHover = new Color(112, 105, 93);
+
+  private Vector2 size;
+
+  public bool Enabled { get; set; }
+
+  public UIToggle(Vector2 position, Vector2 size) : base(position) {
+    this.size = size;
+    Enabled = false;
+    Input.OnLeftMouseClicked += OnClick;
+  }
+
+  private void OnClick(float x, float y) {
+    if (x >= position.X && y >= position.Y && x <= position.X + size.X && y <= position.Y + size.Y) {
+      Enabled = !Enabled;
+    }
+  }
+
+  public override void Render() {
+    var batch = BattleshipGame.Instance.Batch;
+    batch.Draw(ResourceManager.UISurface, position, null, Color.Black, 0f, Vector2.Zero, size, SpriteEffects.None, 0f);
+    Color foregroundColor = !Enabled ? foreground : foregroundHover;
+    batch.Draw(ResourceManager.UISurface, position + new Vector2(2), null, foregroundColor, 0f, Vector2.Zero, size - new Vector2(4), SpriteEffects.None, 0f);
+  }
+}
