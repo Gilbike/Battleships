fe95b59c915ac2a38543172fc54819d306910f33
Gilbike <marton.dikacz@gmail.com>
2023-06-10 18:33:04 +0200

    add connect button

--- src/UI/MultiplayerMenu.cs
+++ src/UI/MultiplayerMenu.cs
@@ -4,9 +4,10 @@ using Microsoft.Xna.Framework;
 namespace Battleships.UI;
 
 public class MultiplayerMenu : UIScreen {
-  private static int totalHeight = 35 + 2 + 35 + 30 + 2 + 35 + 30 + 30;
+  private static int totalHeight = 35 + 2 + 35 + 30 + 2 + 35 + 30 + 2 + 30 + 2 + 30;
 
   private UIButton backButton;
+  private UIButton startButton;
   private UIEditbox ipInput;
   private UIEditbox nameInput;
 
@@ -23,11 +24,14 @@ public class MultiplayerMenu : UIScreen {
     Elements.Add(new UILabel(UIManager.ScreenCenter - new Vector2(150, settingsTitleSize.Y / 2 + totalHeight / 2) + new Vector2(0, 104), "Your name"));
     nameInput = new UIEditbox(UIManager.ScreenCenter - new Vector2(150, settingsTitleSize.Y / 2 + totalHeight / 2) + new Vector2(0, 132), new Vector2(300, 30));
 
+    startButton = new UIButton(UIManager.ScreenCenter - new Vector2(100, settingsTitleSize.Y / 2 - (totalHeight / 2) + 62), new Vector2(200, 30), "Connect");
+
     backButton = new UIButton(UIManager.ScreenCenter - new Vector2(100, settingsTitleSize.Y / 2 - (totalHeight / 2) + 30), new Vector2(200, 30), "Back");
     backButton.OnButtonPressed += delegate () { BackRequested?.Invoke(); };
 
     Elements.Add(ipInput);
     Elements.Add(nameInput);
+    Elements.Add(startButton);
     Elements.Add(backButton);
   }
 }
