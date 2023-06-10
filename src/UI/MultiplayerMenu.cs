181761fe4ee36b6bf4ea13f9b61e800e6e95dacf
Gilbike <marton.dikacz@gmail.com>
2023-06-10 18:29:44 +0200

    add multiplayer menu ui

--- src/UI/MultiplayerMenu.cs
+++ src/UI/MultiplayerMenu.cs
@@ -4,9 +4,11 @@ using Microsoft.Xna.Framework;
 namespace Battleships.UI;
 
 public class MultiplayerMenu : UIScreen {
-  private static int totalHeight = 35 + 2 + 30;
+  private static int totalHeight = 35 + 2 + 35 + 30 + 2 + 35 + 30 + 30;
 
   private UIButton backButton;
+  private UIEditbox ipInput;
+  private UIEditbox nameInput;
 
   public Action BackRequested;
 
@@ -14,9 +16,18 @@ public class MultiplayerMenu : UIScreen {
     Vector2 settingsTitleSize = Resources.ResourceManager.Font.MeasureString("Multiplayer");
     Elements.Add(new UILabel(UIManager.ScreenCenter - new Vector2(settingsTitleSize.X / 2, settingsTitleSize.Y / 2 + totalHeight / 2), "Multiplayer"));
 
+    Elements.Add(new UILabel(UIManager.ScreenCenter - new Vector2(150, settingsTitleSize.Y / 2 + totalHeight / 2) + new Vector2(0, 37), "Server IP Address"));
+    ipInput = new UIEditbox(UIManager.ScreenCenter - new Vector2(150, settingsTitleSize.Y / 2 + totalHeight / 2) + new Vector2(0, 68), new Vector2(300, 30));
+    ipInput.Text = "127.0.0.1";
+
+    Elements.Add(new UILabel(UIManager.ScreenCenter - new Vector2(150, settingsTitleSize.Y / 2 + totalHeight / 2) + new Vector2(0, 104), "Your name"));
+    nameInput = new UIEditbox(UIManager.ScreenCenter - new Vector2(150, settingsTitleSize.Y / 2 + totalHeight / 2) + new Vector2(0, 132), new Vector2(300, 30));
+
     backButton = new UIButton(UIManager.ScreenCenter - new Vector2(100, settingsTitleSize.Y / 2 - (totalHeight / 2) + 30), new Vector2(200, 30), "Back");
     backButton.OnButtonPressed += delegate () { BackRequested?.Invoke(); };
 
+    Elements.Add(ipInput);
+    Elements.Add(nameInput);
     Elements.Add(backButton);
   }
 }
